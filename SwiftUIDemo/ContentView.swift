e2e2f4488765fe07368c88bcc6dadb7ae1d8badb
James Porter <james@amimetic.co.uk>
2019-10-16 20:04:14 +0100

    Initial Commit

---
+++ SwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by James Porter on 16/10/2019.
+//  Copyright © 2019 James Porter. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
