a3c1628938caa1eb1b58a39a466f1bbf986eb702
James Porter <james@amimetic.co.uk>
2019-10-17 16:13:10 +0100

    where we got to in session today

--- SwiftUIDemo/ContentView.swift
+++ SwiftUIDemo/ContentView.swift
@@ -1,8 +1,7 @@
 import SwiftUI
 
-
 struct ContentView: View {
-    
+
     @State var text: String = ""
     @ObservedObject var state: Store
     
@@ -10,29 +9,87 @@ struct ContentView: View {
         NavigationView {
             VStack {
                 HStack {
-                    TextField("Item", text: $text)
+                    TextField("To Do Item", text: $text).padding()
                     Button(action: {
-                        self.state.addToDo(self.text)
+                        self.state.addItem(text: self.text)
                         self.text = ""
                     }) {
-                        Text("Add")
-                    }
-                }.padding()
-                List(state.items, id: \.id) { item in
-                    HStack {
-                        Text(item.description)
-                            .foregroundColor(item.done ? .gray : .green)
-                        Spacer()
-                        Button(action: { self.state.toggle(id: item.id)
-                        }) {
-                            Text(item.done ? "Set Not Done": "Set Done").padding()
-                        }
+                        Text("Add").padding().background(Color.yellow).cornerRadius(8.0).padding()
                     }
                 }
-            }.navigationBarTitle("To Do")
+                List(state.items, id: \.id) { item in
+                    Text(item.text).foregroundColor( item.done ? .green : .red).padding()
+                    Spacer()
+                    Button(action: {
+                        self.state.toggleDone(id: item.id)
+                    }) {
+                        Text("Toggle").padding().background(Color.blue).cornerRadius(8.0).padding()
+                    }
+                }
+            }.navigationBarTitle("To Do App")
         }
     }
 }
+
+
+extension View {
+    func greened() -> some View {
+        self.foregroundColor(.green)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//struct ContentView: View {
+//
+//    @State var text: String = ""
+//    @ObservedObject var state: Store
+//
+//    var body: some View {
+//        NavigationView {
+//            VStack {
+//                HStack {
+//                    TextField("Item", text: $text)
+//                    Button(action: {
+//                        self.state.addToDo(self.text)
+//                        self.text = ""
+//                    }) {
+//                        Text("Add")
+//                    }
+//                }.padding()
+//                List(state.items, id: \.id) { item in
+//                    HStack {
+//                        Text(item.description)
+//                            .foregroundColor(item.done ? .gray : .green)
+//                        Spacer()
+//                        Button(action: { self.state.toggle(id: item.id)
+//                        }) {
+//                            Text(item.done ? "Set Not Done": "Set Done").padding()
+//                        }
+//                    }
+//                }
+//            }.navigationBarTitle("To Do")
+//        }
+//    }
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- SwiftUIDemo/Data.swift
+++ SwiftUIDemo/Data.swift
@@ -1,23 +1,81 @@
 import SwiftUI
 
+
+
+// 1. struct ToDoItem {
+
 struct ToDoItem {
-    var description: String = ""
+    var text: String
     var id: Int
     var done: Bool = false
 }
 
+//let item = ToDoItem(text: "do demo", id: 1)
+
+
+
+
+// 2. class Store : ObservableObject {
+
 class Store : ObservableObject {
     @Published var items: [ToDoItem] = []
     
-    func addToDo(_ desc: String) {
-        items.append(ToDoItem(description: desc, id: items.count))
+    func addItem(text: String) {
+        items.append(ToDoItem(text: text, id: items.count))
         self.objectWillChange.send()
     }
     
-    func toggle(id: Int) {
-        if let idx = items.firstIndex(where: { i in i.id == id}) {
+    func toggleDone(id: Int) {
+        if let idx = items.firstIndex(where: { it in
+            it.id == id
+            }) {
             items[idx].done.toggle()
-            self.objectWillChange.send()
         }
+        self.objectWillChange.send()
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//struct ToDoItem {
+//    var description: String = ""
+//    var id: Int
+//    var done: Bool = false
+//}
+//
+
+
+
+
+
+//class Store : ObservableObject {
+//    @Published var items: [ToDoItem] = []
+//
+//    func addToDo(_ desc: String) {
+//        items.append(ToDoItem(description: desc, id: items.count))
+//        self.objectWillChange.send()
+//    }
+//
+//    func toggle(id: Int) {
+//        if let idx = items.firstIndex(where: { i in i.id == id}) {
+//            items[idx].done.toggle()
+//            self.objectWillChange.send()
+//        }
+//    }
+//}
